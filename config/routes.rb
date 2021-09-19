31d8cb932e69f3aafbcad8bd7ad4e3a115c06fca
Kei-9999 <fighting_dooma@yahoo.co.jp>
2021-09-19 14:41:35 +0000

    919

--- config/routes.rb
+++ config/routes.rb
@@ -17,8 +17,8 @@ resources :users do
  get 'relationships/follow' => "relationships#follow", as: "relationship_follow"
  get 'relationships/follower' => "relationships#follower", as: "relationship_follower"
   resource :relationships
+ # end
 end
-
  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resources :posts do
  resources :comments, only: [:create, :destroy]
@@ -32,9 +32,7 @@ resource :likes, only: [:create, :destroy]
 
     resources :users,only: [:index,:show,:edit,:destroy]
     resources :posts,only: [:index,:show,:edit,:destroy]
-    # resources :clients,only: [:index,:show,:edit,:update]
-    # resources :orders,only: [:index,:show,:update]
-    # resources :order_items,only: [:update]
+
     # get '/search', to: 'searches#search'
 
 end
