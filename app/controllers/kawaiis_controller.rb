1357eb18c3949763124b9034444c76035a2fc3d3
Kei-9999 <fighting_dooma@yahoo.co.jp>
2021-09-21 14:24:27 +0000

    finish

--- app/controllers/kawaiis_controller.rb
+++ app/controllers/kawaiis_controller.rb
@@ -2,8 +2,14 @@ class KawaiisController < ApplicationController
 
     def create
      @post = Post.find(params[:post_id])
-     kawaii = current_user.kawaiis.new(post_id: @post.id)
+     
+     kawaii = Kawaii.new
+     kawaii.post_id = @post.id
+     kawaii.user_id = current_user.id
+     
+     #kawaii = current_user.kawaiis.new(post_id: @post.id)
      kawaii.save
+     
     end
     
 end
